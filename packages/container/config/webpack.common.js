c883959ea0fac24090f9c66a6db0624cb7d783d0
chandra-prakash-soni <cprakash1490@gmail.com>
2025-06-13 14:06:09 +0530

    created container deployment script

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,17 +1,24 @@
-module.exports={
-    module:{
-        rules:[
-            {
-                test:/\.m?js$/,
-                exclude:/node_modules/,
-                use:{
-                    loader:'babel-loader',
-                    options:{
-  presets: ["@babel/preset-env", "@babel/preset-react"],
-  plugins: ["@babel/plugin-transform-runtime"]  
-}
-                }
-            }
-        ]
-    }
-}
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/, // for .js and .mjs files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-transform-runtime"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ]
+};
--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,12 +1,7 @@
 const {merge} = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-
-
-
- const packageJson = require('../package.json');
-
-
+const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common');
 
 const devConfig = {
@@ -25,9 +20,7 @@ const devConfig = {
             },
              shared: packageJson.dependencies,
         }),
-        new HtmlWebpackPlugin({
-            template:"./public/index.html"
-        })
+       
     ]
 }
 
