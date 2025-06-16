8352c318200e4d9a6eb5597f720339adb1826d12
chandra-prakash-soni <cprakash1490@gmail.com>
2025-06-16 13:24:51 +0530

    fix

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -6,7 +6,7 @@ const commonConfig = require("./webpack.common");
 const prodConfig = {
     mode:"production",
     output:{
-        filename:"[name].[contenthast].js",
+        filename:"[name].[contenthash].js",
         publicPath:"/marketing/latest/"
     },
     plusin:[
